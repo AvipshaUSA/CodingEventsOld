f3d4a6e433fdf6fc35cb5794cdfb0cd02f8aa166
AvipshaUSA <68029807+AvipshaUSA@users.noreply.github.com>
2020-11-08 19:19:49 -0600

    added a Unique Id

--- Models/Event.cs
+++ Models/Event.cs
@@ -13,11 +13,18 @@ namespace CodingEvents.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Date;
+        public int Id { get; }
+        private static int nextId = 1; 
+        
+
+    
         public Event(string name, string description, string date) // constructor
         {
             Name = name;
             Description = description;
             Date = date;
+            Id = nextId;
+            nextId++;
         }
 
         public override string ToString()
